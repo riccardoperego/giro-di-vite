a3aad8949bd2570b70348bfaf9bd09c14378bfee
Riccardo Perego <riccardoperego88@gmail.com>
2019-12-28 23:18:37 +0100

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  inAppBroweserOptions: InAppBrowserOptions | string;
 
+  constructor(private iab: InAppBrowser) {}
+
+  ngOnInit(): void {
+
+    // this.inAppBroweserOptions = {
+    //   location: 'no',
+    //   zoom: 'no',
+    //   hideurlbar: 'yes',
+    //   hidenavigationbuttons: 'yes',
+    // };
+
+    this.inAppBroweserOptions = 'location=no,hideurlbar=yes,hidenavigationbuttons=yes';
+    console.log('OnInit!');
+  }
+
+  ionViewDidEnter(): void {
+    this.iab.create(
+      'https://sites.google.com/view/plg-girodivite/home',
+      '_self',
+      this.inAppBroweserOptions
+    );
+    console.log('did enter!');
+
+  }
 }
